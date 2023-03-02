63a21d9ac9e3d942b770b37abddc9bab02de65e9
Khaoula <kerrou29@uw.edu>
2023-03-01 17:09:24 -0800

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,77 @@
+// import { Component } from "react";
+// import logo from './logo.svg';
+// import './App.css';
+
+// class App extends Component {
+//   state = {
+//     patients: []
+//   };
+
+//   async componentDidMount() {
+//     const response = await fetch('/api/patients/getPatients');
+//     const body = await response.json();
+//     this.setState({patients: body});
+//   }
+
+//   render() {
+//     const {patients} = this.state;
+//     return (
+//         <div className="App">
+         
+//             <div className="App-intro">
+//               <h1 className="text-center">Patients</h1>
+//               <table className = "table table-striped">
+//                 <thead>
+//                   <tr>
+//                     <td>Patient Id</td>
+//                     <td>Patient Name</td>
+//                     <td>Patient Email</td>
+//                     <td>Patient Dob</td>
+//                     <td>Patient Age</td>
+
+//                   </tr>
+//                 </thead>
+//                 <tbody>
+//                   {patients.map(
+//                     patient =>
+//                     <tr key={patient.id}>
+//                       <td>{patient.id}</td>
+//                       <td>{patient.name}</td>
+//                       <td>{patient.email}</td>
+//                       <td>{patient.dob} </td>
+//                       <td>{patient.age}</td>
+//                     </tr>
+//                 )}
+//                 </tbody>
+//               </table>
+              
+//             </div>
+//         </div>
+//     );
+//   }
+// }
+// export default App;
+
+import React, { Component } from 'react';
+import './App.css';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import PatientList from './PatientList';
+import PatientEdit from "./PatientEdit";
+
+class App extends Component {
+  render() {
+    return (
+        <Router>
+          <Switch>
+            <Route path='/' exact={true} component={Home}/>
+            <Route path='/getPatients/' exact={true} component={PatientList}/>
+            <Route path='/api/patients/updatePatient/:id' component={PatientEdit}/>
+          </Switch>
+        </Router>
+    )
+  }
+}
+
+export default App;
+
---
+++ src/components/PatientList.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import { Button, ButtonGroup, Container, Table } from 'reactstrap';
+import AppNavbar from './AppNavbar';
+import { Link } from 'react-router-dom';
+
+
+class PatientList extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {patients: []};
+        this.remove = this.remove.bind(this);
+    }
+
+
+    componentDidMount() {
+        fetch('/api/patients/getPatients')
+            .then(response => response.json())
+            .then(data => this.setState({patients: data}));
+    }
+    async remove(id) {
+        await fetch(`/api/patients/deletePatient/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        }).then(() => {
+            let updatedPatients = [...this.state.patients].filter(i => i.id !== id);
+            this.setState({patients: updatedPatients});
+        });
+    }
+    
+    render() {
+
+        const {patients, isLoading} = this.state;
+    
+        if (isLoading) {
+            return <p>Loading...</p>;
+        }
+    
+        const patientList = patients.map(patient => {
+            return <tr key={patient.id}>
+                <td style={{whiteSpace: 'nowrap'}}>{patient.name}</td>
+                <td>{patient.email}</td>
+                <td>{patient.dob}</td>
+                <td>{patient.age}</td>
+                <td>
+                    <ButtonGroup>
+                        <Button size="sm" color="primary" tag={Link} to={"/api/patients/updatePatient/" + patient.id}>Edit</Button>
+                        <Button size="sm" color="danger" onClick={() => this.remove(patient.id)}>Delete</Button>
+                    </ButtonGroup>
+                </td>
+            </tr>
+        });
+    
+        return (
+            <div>
+                <AppNavbar/>
+                <Container fluid>
+                    <div className="float-right">
+                        <Button color="success" tag={Link} to="/getPatients/addPatient" >Add Patient</Button>
+                    </div>
+                    <h3>Patients</h3>
+                    <Table className="mt-4">
+                        <thead>
+                        <tr>
+                            <th width="30%">Name</th>
+                            <th width="30%">Email</th>
+                            <th width="30%">Dob</th>
+                            <th width="30%">Age</th>
+                            <th width="40%">Actions</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {patientList}
+                        </tbody>
+                    </Table>
+                </Container>
+            </div>
+        );
+    }
+}
+export default PatientList;
